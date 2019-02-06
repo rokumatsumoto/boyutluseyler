c2e766ea85f96c1dc68cbc464762343ca4eaddb2
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-06 13:07:49 +0300

    Configure Shoulda Matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 
 require 'rspec/rails'
 require 'pundit/matchers'
+require 'shoulda/matchers'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -136,4 +138,11 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
