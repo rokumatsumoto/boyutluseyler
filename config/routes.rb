74de75d38d71dbe7b83cfe5a8218e3ea6c3cef83
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-07 02:31:45 +0300

    Configure routes for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', controllers: { registrations: :registrations,
+                                              passwords: :passwords,
+                                              sessions: :sessions,
+                                              confirmations: :confirmations,
+                                              unlocks: :unlocks }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
