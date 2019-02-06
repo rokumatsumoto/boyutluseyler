4e027738ae51f72fab197db37605c8608e8f7222
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-06 13:17:02 +0300

    Configure timecop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,6 +52,10 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.before(:suite) do
+    Timecop.safe_mode = true
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
