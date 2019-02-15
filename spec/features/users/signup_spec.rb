5dd7583b6493d03d742178354d78290ab263a3c2
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-15 18:24:43 +0300

    Add useful failure messages for PageReload

--- spec/features/users/signup_spec.rb
+++ spec/features/users/signup_spec.rb
@@ -55,11 +55,10 @@ RSpec.describe 'Signup' do
     it 'does not reload the page if the username already exists' do
       existing_user = create(:user)
 
-      fill_in 'user_username', with: existing_user.username
+      fill_in 'user_username',              with: existing_user.username
       fill_in 'user_email',                 with: new_user.email
       fill_in 'user_password',              with: new_user.password
       fill_in 'user_password_confirmation', with: new_user.password
-      wait_for_requests
 
       expect_page_to_not_reload do
         click_button 'btn_sign_up'
--- spec/support/helpers/page_reload.rb
+++ spec/support/helpers/page_reload.rb
@@ -11,13 +11,13 @@ module PageReload
     init_reload_check
     yield
     sleep 0.01
-    expect(not_reloaded?).to be_truthy
+    expect(not_reloaded?).to be_truthy, 'expected page to not reload, got page reload'
   end
 
   def expect_page_to_reload
     init_reload_check
     yield
     sleep 0.01
-    expect(not_reloaded?).to be_falsy
+    expect(not_reloaded?).to be_falsy, 'expected page to reload, got page not reload'
   end
 end
