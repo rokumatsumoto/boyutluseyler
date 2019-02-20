0a32f82ca987af6aa1390a48be58ddcf9696f514
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-02-20 03:23:46 +0300

    Check expired reset password tokens and redirect to new_user_password_url

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -12,14 +12,47 @@ class PasswordsController < Devise::PasswordsController
   # end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+    reset_password_token = Devise.token_generator.digest(
+      User,
+      :reset_password_token,
+      resource.reset_password_token
+    )
+
+    unless reset_password_token.nil?
+      user = User.where(
+        reset_password_token: reset_password_token
+      ).first_or_initialize
+
+      unless user.reset_password_period_valid?
+        flash[:alert] = expired_message_for_reset_password_token
+        redirect_to(new_user_password_url(user_email: user['email']))
+      end
+    end
+  end
 
   # PUT /resource/password
-  # def update
-  #   super
-  # end
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      if Devise.sign_in_after_reset_password
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message!(:notice, flash_message)
+        resource.after_database_authentication
+        sign_in(resource_name, resource)
+      else
+        set_flash_message!(:notice, :updated_not_active)
+      end
+      respond_with resource, location: after_resetting_password_path_for(resource)
+    else
+      flash[:alert] = expired_message_for_reset_password_token
+      redirect_to(new_user_password_url(user_email: resource['email']))
+    end
+  end
 
   # protected
 
@@ -31,4 +64,11 @@ class PasswordsController < Devise::PasswordsController
   # def after_sending_reset_password_instructions_path_for(resource_name)
   #   super(resource_name)
   # end
+
+  private
+
+  def expired_message_for_reset_password_token
+    t('activerecord.attributes.user.reset_password_token') + ' ' +
+      t('errors.messages.expired')
+  end
 end
