714a1d840f2c57e5df20c23d51f30e47e3304244
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-05-29 22:09:43 +0300

    Fix Vue.js production mount template issue
    Vue.js app works in development but not mounting template in production
    
    Issue: https://github.com/rails/webpacker/issues/1520
    Fix: https://github.com/rails/webpacker/issues/1520#issuecomment-436548736

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -20,8 +20,6 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
     policy.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
-  else
-    policy.script_src :self, :https
   end
 end
 
