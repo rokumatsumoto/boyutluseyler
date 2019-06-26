a5c5b212d553edaf125ea8968225624fb3c4989a
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-06-26 23:42:02 +0300

    Redirect unauthorized axios requests to login page
    - Only authenticated users can upload files, redirect user to login page gently
      if the user is not authenticated or session expired.
    
    issue #5

--- app/javascript/lib/utils/axios_utils.js
+++ app/javascript/lib/utils/axios_utils.js
@@ -24,6 +24,31 @@ axios.interceptors.response.use(
   e => {
     window.activeAxios -= 1;
 
+    if (e.response.status === 401) {
+      const $notyNotification = document.getElementById('noty_notification');
+      if ($notyNotification === null) {
+        import('noty')
+          .then((Noty) => {
+            /* eslint-disable-next-line new-cap */
+            new Noty.default({
+              type: 'warning',
+              layout: 'top',
+              id: 'noty_notification',
+              timeout: 4000,
+              progressBar: true,
+              text: "Oturumunuzun süresi doldu, giriş sayfasına yönlendiriliyorsunuz.",
+              callbacks: {
+                afterClose() {
+                  window.location.href = '/sign_in';
+                },
+              },
+            }).show();
+          })
+          .catch(() => {
+            window.location.href = '/sign_in';
+          });
+      }
+    }
     return Promise.reject(e);
   },
 );
