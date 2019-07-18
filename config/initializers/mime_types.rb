53062d1c8d4f686506352a696eb7f4f04ac9d128
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-07-18 19:44:23 +0300

    Add reference link for mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,5 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+# https://github.com/rails/rails/blob/b2eb1d1c55a59fee1e6c4cba7030d8ceb524267c/actionpack/lib/action_dispatch/http/mime_types.rb
