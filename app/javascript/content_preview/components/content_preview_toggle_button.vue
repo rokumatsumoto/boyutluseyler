9f55bd99d7e15d977fc8d6b6dcb4fc81f8622b80
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-07-22 19:19:05 +0300

    feat: Create content preview toggle button component
    Issue: #10

---
+++ app/javascript/content_preview/components/content_preview_toggle_button.vue
@@ -0,0 +1,51 @@
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'ContentPreviewToggleButton',
+  computed: {
+    ...mapState(['activeContent', 'contents']),
+    contentCount() {
+      return this.contents.length;
+    },
+  },
+  mounted() {},
+  methods: {
+    ...mapActions(['setActiveContent']),
+    toggleButtonText(text) {
+      let name = '';
+      if (this.contentCount === 2) {
+        name = this.contents.find(c => c !== this.activeContent).toggleButtonText;
+      } else {
+        name = text;
+      }
+      return name;
+    },
+    changeContent(content) {
+      let newContent = this.activeContent;
+      if (this.contentCount === 2) {
+        newContent = this.contents.find(c => c !== this.activeContent);
+      } else {
+        newContent = content;
+      }
+      this.setActiveContent(newContent);
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <div v-if="contentCount === 2">
+      <button class="btn-sm btn-main" @click="changeContent">
+        {{ toggleButtonText() }}
+      </button>
+    </div>
+    <div v-else-if="contentCount > 2">
+      <div v-for="(content, index) in contents" :key="index">
+        <button class="btn-sm btn-main float-left" @click="changeContent(content)">
+          {{ toggleButtonText(content.toggleButtonText) }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
