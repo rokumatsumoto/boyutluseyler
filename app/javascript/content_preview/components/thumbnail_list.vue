e3f024090e6f98eed893581b9e43a2b4d172c57d
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-07-24 14:03:42 +0300

    feat: Create thumbnail list component
    - Thumbnails place on rows. There is no limit for rows.
    - Every row contains maximum five thumbnails for content preview component.
    
    issue #9

---
+++ app/javascript/content_preview/components/thumbnail_list.vue
@@ -0,0 +1,45 @@
+<script>
+import BaseImg from 'vue_shared/components/base_img.vue';
+
+export default {
+  name: 'ThumbnailList',
+  components: {
+    BaseImg,
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      validator: (prop) => [
+        'url',
+        'imageUrl',
+        ].every( e => Object.keys(prop[0]).includes(e))
+    },
+    imgLoading: {
+      type: String,
+      default: 'auto',
+      required: false,
+    },
+    imgAltTag: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+};
+</script>
+<template>
+  <div style="width: 100%;">
+    <a
+      v-for="(item, i) in list"
+      :key="i"
+      class="thumb"
+      :href="item.url"
+      target="_blank"
+      rel="noopener"
+      @click.prevent="$emit('img-update', item)"
+    >
+      <base-img :loading="imgLoading" :alt="imgAltTag" :src="item.imageUrl" />
+    </a>
+</div>
+</template>
