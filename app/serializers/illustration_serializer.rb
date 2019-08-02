177688bf4784a8a35d54ed3a534699b91e5d97ea
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-08-02 17:08:36 +0300

    Annotate serializers

--- app/serializers/illustration_serializer.rb
+++ app/serializers/illustration_serializer.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: illustrations
+#
+#  id           :bigint(8)        not null, primary key
+#  url          :string           not null
+#  url_path     :string           not null
+#  size         :integer          not null
+#  content_type :string           not null
+#  image_url    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class IllustrationSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
