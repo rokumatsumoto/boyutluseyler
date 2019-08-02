e6d51d11e98d42b748f37c84f29b5a41ffabf89a
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-08-02 17:08:36 +0300

    Annotate serializers

--- app/serializers/blueprint_serializer.rb
+++ app/serializers/blueprint_serializer.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: blueprints
+#
+#  id           :bigint(8)        not null, primary key
+#  url          :string           not null
+#  url_path     :string           not null
+#  size         :integer          not null
+#  content_type :string           not null
+#  image_url    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class BlueprintSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
