f684cb0fe6189e34c3e54a0ff8b5297d162db628
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-08-03 23:27:30 +0300

    Move ensure_content_type_correct method into FileValidations concern
    issue #18

--- app/helpers/aws_s3_uploader_helper.rb
+++ app/helpers/aws_s3_uploader_helper.rb
@@ -8,7 +8,7 @@ module AWSS3UploaderHelper
       key_starts_with: key_starts_with(type),
       content_type_starts_with: content_type_starts_with(type),
       success_action_status: Rails.application.credentials.aws_s3[:success_action_status],
-      acl: object_acl,
+      acl: Rails.application.credentials.aws_s3[:acl],
       signature_expiration: (Time.now.utc + 15.minutes), # TODO: constants
       cache_control: 'public, max-age=315576000', # TODO: constants
       content_length_range: 1..104_857_600 # TODO: constants
@@ -50,40 +50,5 @@ module AWSS3UploaderHelper
     when :blueprint
       ''
     end
-  end
-
-  def allowed_content_types(type)
-    case type
-    when :illustration
-      %w[image/png image/gif image/jpeg]
-    when :blueprint
-      %w[application/vnd.ms-pki.stl
-         model/stl
-         image/x-3ds
-         application/x-tgif
-         application/zip]
-    end
-  end
-
-  def validate_content_type(type, obj_file_name, obj_content_type)
-    invalid = obj_content_type.blank? || allowed_content_types(type).none?(obj_content_type)
-
-    return content_type_by_filename(obj_file_name) if invalid
-
-    obj_content_type
-  end
-
-  def content_type_by_filename(filename)
-    mime_info = MiniMime.lookup_by_filename(filename)
-
-    return mime_info.content_type unless mime_info.nil?
-
-    ''
-  end
-
-  private
-
-  def object_acl
-    Rails.application.credentials.aws_s3[:acl]
   end
 end
--- app/models/concerns/file_validations.rb
+++ app/models/concerns/file_validations.rb
@@ -14,15 +14,16 @@ module FileValidations
     validates :url_path, presence: true
     validates_inclusion_of :size, in: 1..104_857_600 # TODO: constants
     before_validation :sanitize_attrs
-  end
-
-  def filename_without_extension
-    File.basename(url_path, '.*').split('.')[0]
+    before_validation :ensure_content_type_correct
   end
 
   def filename_is_blank
     # user input: '.stl', '.png', '  .zip'
     errors.add(:url_path, :blank) if filename_without_extension.blank?
+  end
+
+  def filename_without_extension
+    File.basename(url_path, '.*').split('.')[0]
   end
 
   def sanitize_attrs
@@ -31,5 +32,34 @@ module FileValidations
       value = self[attr]
       self[attr] = Sanitize.fragment(value) if value.present?
     end
+  end
+
+  def ensure_content_type_correct
+    invalid = content_type.blank? || allowed_content_types.none?(content_type)
+
+    self.content_type = content_type_by_filename(url_path) if invalid
+  end
+
+  def allowed_content_types
+    case model_name.name
+    when 'Illustration'
+      %w[image/png image/gif image/jpeg]
+    when 'Blueprint'
+      %w[application/vnd.ms-pki.stl
+         model/stl
+         image/x-3ds
+         application/x-tgif
+         application/zip]
+    else
+      []
+    end
+  end
+
+  def content_type_by_filename(filename)
+    mime_info = MiniMime.lookup_by_filename(filename)
+
+    return mime_info.content_type unless mime_info.nil?
+
+    ''
   end
 end
