44a054f22d3c1e44924429dcfde3b827467d781f
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-08-25 14:19:21 +0300

    Change username validator Vuex structure to ES6
    - Enable jquery environment for ESLint
    issue #22

--- app/javascript/pages/profiles/edit/index.js
+++ app/javascript/pages/profiles/edit/index.js
@@ -1,1 +1,12 @@
-import 'pages/shared/username_validator';
+import UsernameValidator from 'pages/shared/username_validator'
+
+document.addEventListener('turbolinks:load', () => {
+  new UsernameValidator(); // eslint-disable-line no-new
+});
+
+if (typeof Turbolinks === 'undefined' || Turbolinks === null) {
+  location.reload;
+}
+Turbolinks.dispatch('turbolinks:load');
+
+
