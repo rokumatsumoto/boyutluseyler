3bdc868129047b3cb0b925b1c316f42cf332aeb7
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-09-04 15:45:14 +0300

    Remove Swiper config from webpack
    issue #32

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const swiperConfig = require('./swiper_config')
-
-environment.config.merge(swiperConfig);
 
 module.exports = environment.toWebpackConfig()
