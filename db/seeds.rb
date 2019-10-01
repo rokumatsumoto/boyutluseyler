e46499ebd6fad003bd81170a89b8e7ded711df9f
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-10-01 19:16:59 +0300

    Configure database seeding
    - Add seeds for Category entity

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Load seeds from YAML files in db/seeds/*
+
+require 'seed_file'
+
+SEED_DIR = 'db/seeds'
+
+Dir[Rails.root.join("#{SEED_DIR}/*.yml")].each do |f|
+  seed = SeedFile.new(f)
+  puts "==> Seeding: #{seed.model.name}"
+  print "  "
+  seed.import! { |key| print "#{key} | " }
+  print "\n"
+end
