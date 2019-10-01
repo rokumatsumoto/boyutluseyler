f22bf6c95d02893834942550522836cb75a60e98
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-10-01 15:42:20 +0300

    Add slug migration for Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_204858) do
+ActiveRecord::Schema.define(version: 2019_10_01_110550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_09_30_204858) do
     t.integer "list_order", limit: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
   create_table "design_blueprints", force: :cascade do |t|
