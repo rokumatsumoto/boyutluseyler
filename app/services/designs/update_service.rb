92fa81c2b02ce8236fad6a9aaea58eaa57cc3431
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-10-24 22:24:07 +0300

    Change design download state when blueprints change

--- app/services/designs/update_service.rb
+++ app/services/designs/update_service.rb
@@ -5,8 +5,12 @@ module Designs
     def execute
       before_update
 
-      design.update(params)
+      success = design.update(params)
 
+      after_update if success
+
+      # it must be executed whether update successful or not
+      # for already saved files
       Designs::Files::MoveService.new(design, current_user, params).execute
 
       design
@@ -14,6 +18,24 @@ module Designs
 
     def before_update
       design.model_file_format = model_file_format_for(design)
+
+      if blueprints_changed?
+        design_download.set_step_as(:file_updated)
+      end
+    end
+
+    def after_update
+      if blueprints_changed?
+        Designs::Downloads::UpdateService.new(nil, nil, url: '').execute(design_download)
+      end
+    end
+
+    def blueprints_changed?
+      @blueprints_changed ||= !blueprint_ids_identical?(design)
+    end
+
+    def design_download
+      @design_download ||= DesignDownload.find_by(design_id: design.id)
     end
   end
 end
