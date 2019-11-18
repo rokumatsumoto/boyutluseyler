8ff415898a2cf482165a1d44ad06696fd6ae92d2
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-11-18 21:59:03 +0300

    Annotate files related designs table

--- app/models/design.rb
+++ app/models/design.rb
@@ -3,19 +3,20 @@
 #
 # Table name: designs
 #
-#  id                :bigint(8)        not null, primary key
-#  name              :string(120)      not null
-#  description       :text             not null
-#  printing_settings :text
-#  model_file_format :string
-#  license_type      :string           not null
-#  allow_comments    :boolean          default(TRUE), not null
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  user_id           :bigint(8)        not null
-#  category_id       :bigint(8)        not null
-#  slug              :string
-#  downloads_count   :integer          default(0), not null
+#  id                     :bigint(8)        not null, primary key
+#  name                   :string(120)      not null
+#  description            :text             not null
+#  printing_settings      :text
+#  model_file_format      :string
+#  license_type           :string           not null
+#  allow_comments         :boolean          default(TRUE), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  user_id                :bigint(8)        not null
+#  category_id            :bigint(8)        not null
+#  slug                   :string
+#  downloads_count        :integer          default(0), not null
+#  hourly_downloads_count :float            default(0.0), not null
 #
 
 class Design < ApplicationRecord
