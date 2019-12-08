69d6283a32c596d8833e3e1c5dacfbecdbe09140
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-12-08 23:17:06 +0300

    Disable spring when using erb loader

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -1,18 +1,11 @@
-/* global process:false */
-
 module.exports = {
   test: /\.erb$/,
   enforce: "pre",
   exclude: /node_modules/,
-
   use: [{
     loader: "rails-erb-loader",
     options: {
-      runner: (/^win/.test(process.platform) ? "ruby " : "") + "bin/rails runner",
-      env: {
-        ...process.env,
-        DISABLE_SPRING: 1,
-      },
+      runner: 'bin/rails runner'
     },
   }],
 }
