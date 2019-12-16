2f3752c284df0d56bc925cca8e0eae4e67354e72
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-12-16 21:39:37 +0300

    Add support for seamless migration
    To make everything to work during all steps and so on we must keep reading
    from old column while it’s still there, start writing into new column when
    it’s created, then read from new column only when data is fully migrated
    and the old column doesn’t exist anymore.

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -25,5 +25,23 @@ class Illustration < ApplicationRecord
   # FileValidations (concern)
   #
 
-  validates_format_of :url_path, with: /\.(png|jpg|jpeg|gif)\z/i
+  validates :url_path, format: { with: /\.(png|jpg|jpeg|gif)\z/i }
+
+  # << REMOVE ON NEXT RELEASE (0.8.0)
+  def thumb_url
+    self.class.reset_column_information if self.class.column_names.include?('image_url')
+    has_attribute?(:image_url) ? image_url : super
+  end
+
+  def thumb_url=(value)
+    self.class.reset_column_information if self.class.column_names.include?('image_url')
+    if has_attribute?(:image_url) && has_attribute?(:thumb_url)
+      self[:image_url] = value
+      return self[:thumb_url] = value
+    end
+    return self[:image_url] = value if has_attribute?(:image_url)
+
+    super
+  end
+  # >> REMOVE ON NEXT RELEASE (0.8.0)
 end
