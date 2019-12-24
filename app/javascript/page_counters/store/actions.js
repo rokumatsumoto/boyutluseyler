29b65dcf78d1d7c1b90d75080934ed5a35d29b71
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-12-24 15:25:35 +0300

    Create likes counter component

--- app/javascript/page_counters/store/actions.js
+++ app/javascript/page_counters/store/actions.js
@@ -2,4 +2,5 @@ import * as types from './mutation_types';
 
 export const setDownloadsCount = ({ commit }, count) => commit(types.SET_DOWNLOADS_COUNT, count);
 
+export const setLikesCount = ({ commit }, count) => commit(types.SET_LIKES_COUNT, count);
 
--- app/javascript/page_counters/store/mutations.js
+++ app/javascript/page_counters/store/mutations.js
@@ -4,4 +4,8 @@ export default {
   [types.SET_DOWNLOADS_COUNT](state, downloadsCount) {
     Object.assign(state, { downloadsCount });
   },
+
+  [types.SET_LIKES_COUNT](state, likesCount) {
+    Object.assign(state, { likesCount });
+  },
 };
