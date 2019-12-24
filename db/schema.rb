9d138825aa6322ed3c7d662a9506c4c2b9fb8fc6
Samet Gunaydin <rokumatsumoto@gmail.com>
2019-12-24 15:21:26 +0300

    Add migrations for design likes count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_111318) do
+ActiveRecord::Schema.define(version: 2019_12_24_000412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_111318) do
     t.float "hourly_downloads_count", default: 0.0, null: false
     t.float "popularity_score", default: 0.0, null: false
     t.datetime "home_popular_at"
+    t.integer "likes_count", default: 0, null: false
     t.index ["category_id"], name: "index_designs_on_category_id"
     t.index ["slug"], name: "index_designs_on_slug", unique: true
     t.index ["user_id"], name: "index_designs_on_user_id"
