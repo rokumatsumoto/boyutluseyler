370ca37df716a2267369972fa4d8abea4e8bcaeb
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-01-24 22:12:55 +0300

    Configure Pundit Matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,8 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 require 'zonebie/rspec'
 require 'sidekiq/testing'
-# TODO: Activate when they are needed
-# require 'pundit/matchers'
-# require 'pundit/rspec'
+require 'pundit/matchers'
+require 'pundit/rspec'
 
 # Check for pending migrations before tests are run
 ActiveRecord::Migration.maintain_test_schema!
