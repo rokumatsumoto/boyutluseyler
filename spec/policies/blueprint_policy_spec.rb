3e6c79f57f96f87e5de6504088b6bfab3142a790
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-01-31 23:00:12 +0300

    Add tests for Blueprint policy

---
+++ spec/policies/blueprint_policy_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe BlueprintPolicy, type: :policy do
+  subject { described_class.new(user, blueprint) }
+
+  let(:existing_user) { create(:user) }
+  let(:blueprint) { build_stubbed(:blueprint) }
+
+  it_behaves_like 'user is not signed-in policy'
+
+  context 'when user uploads a design blueprint file' do
+    context 'with user role' do
+      let(:user) { existing_user }
+
+      it { is_expected.to permit_action(:create) }
+    end
+
+    context 'with admin role' do
+      xit { is_expected.to permit_action(:create) }
+    end
+  end
+end
