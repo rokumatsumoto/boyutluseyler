bc6d45e660a61ff31c6e20f9232d2bec8ae365cc
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-02-03 22:42:48 +0300

    Update sequences

--- spec/factories/sequences.rb
+++ spec/factories/sequences.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   sequence(:username) { |n| "user#{n}" }
-  sequence(:email) { |n| "user#{n}@example.org" }
+  sequence(:email)    { |n| "user#{n}@example.org" }
+  sequence(:title)    { |n| "#{Faker::Book.title}#{n}" }
+  sequence(:slug)     { |n| "slug-#{n}" }
 end
