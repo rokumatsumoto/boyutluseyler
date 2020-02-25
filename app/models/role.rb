596b6038d456f302dd632b492e9c3c92b3e7fe95
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-02-25 20:14:16 +0300

    Annotate files related roles table

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  resource_type :string
+#  resource_id   :bigint(8)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+
 class Role < ApplicationRecord
   ROLES = %w[
     admin
