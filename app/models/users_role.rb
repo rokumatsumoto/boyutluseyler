d256f093a063687f29ada6ae7086fefc7db59071
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-02-25 20:14:54 +0300

    Annotate files related users_roles table

--- app/models/users_role.rb
+++ app/models/users_role.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: users_roles
+#
+#  user_id :bigint(8)
+#  role_id :bigint(8)
+#
+
 class UsersRole < ApplicationRecord
   belongs_to :user
   belongs_to :role
