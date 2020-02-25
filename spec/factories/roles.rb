5a803e2e907e36857495543184e7a3db2bde7259
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-02-25 20:14:16 +0300

    Annotate files related roles table

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  resource_type :string
+#  resource_id   :bigint(8)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryBot.define do
   factory :role do
     
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  resource_type :string
+#  resource_id   :bigint(8)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
