450a1c5e677bfa1cf06fac54da8f00d379a954f0
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-03-10 12:56:58 +0300

    Invalidate category list cache after destroying a category
    Refactor CategoriesHelper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,21 +1,4 @@
 # frozen_string_literal: true
 
 module CategoriesHelper
-  def fetch_categories
-    category_list = Rails.cache.fetch(category_list_cache_key) do
-      Category.order(:list_order).to_json
-    end
-
-    JSON.parse(category_list)
-  end
-
-  def category_list_cache_key
-    last_modified = Category.order(:updated_at).last
-
-    return nil if last_modified.nil?
-
-    last_modified_str = last_modified.updated_at.utc.to_s(:number)
-
-    "all_categories/#{last_modified_str}"
-  end
 end
