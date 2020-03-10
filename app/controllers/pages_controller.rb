1617f7bf2b5e071694c34bff614fa0b7271864e0
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-03-10 13:21:13 +0300

    Refactor Rails helpers
    Refactor DesignsHelper
    Refactor UsersHelper

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  include DesignsHelper
-
   def home
     skip_authorization
 
     # TODO: refactor action
 
-    @most_downloaded = fetch_most_downloaded
+    @most_downloaded = Design.cached_most_downloaded
 
-    @popular_designs = fetch_popular_designs
+    @popular_designs = Design.cached_popular_designs
   end
 end
--- app/helpers/designs_helper.rb
+++ app/helpers/designs_helper.rb
@@ -1,33 +1,6 @@
 # frozen_string_literal: true
 
 module DesignsHelper
-  def fetch_most_downloaded
-    design_list = Rails.cache.fetch('most_downloaded',
-                                    expires_in: Design::HOURLY_DOWNLOAD_INTERVAL) do
-      Designs::Downloads::HourlyDownloadsCountService.new.execute
-
-      Design.most_downloaded_with_illustrations.to_json
-    end
-
-    JSON.parse(design_list)
-  end
-
-  def fetch_popular_designs
-    design_list = Rails.cache.fetch('popular_designs', expires_in: Design::POPULAR_INTERVAL) do
-      Designs::BecomePopularService.new.execute
-
-      Design.home_popular_with_illustrations.to_json
-    end
-
-    JSON.parse(design_list)
-  end
-
-  def liked_design?(design_id)
-    return false unless current_user
-
-    Ahoy::Event.cached_any_events_for?(Ahoy::Event::LIKED_DESIGN, current_user, design_id: design_id)
-  end
-
   def license_title_for(design)
     # TODO: i18n
     return 'Telif Hakkı' if design.license_type == 'license_none'
