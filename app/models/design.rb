eb9312bf7e18b0aa3166df3cf57e74a87661c38f
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-03-14 22:19:05 +0300

    Update validate association syntax for category

--- app/models/design.rb
+++ app/models/design.rb
@@ -57,7 +57,7 @@ class Design < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :license_type, presence: true
-  validates :category_id, presence: true
+  validates :category, presence: true
   validates :design_illustrations, presence: true
   validates :design_blueprints, presence: true
 
