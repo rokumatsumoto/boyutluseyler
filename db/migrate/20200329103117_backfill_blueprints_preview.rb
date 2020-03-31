3a7dc0b15cf51ce601c644d844b6a20d47228c89
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-03-31 14:42:18 +0300

    Prefer find_each over find_in_batches for code readability

--- db/migrate/20200329103117_backfill_blueprints_preview.rb
+++ db/migrate/20200329103117_backfill_blueprints_preview.rb
@@ -2,18 +2,12 @@ class BackfillBlueprintsPreview < ActiveRecord::Migration[5.2]
   disable_ddl_transaction!
 
   def up
-    Blueprint.where(content_type: Blueprint::PREVIEW_CONTENT_TYPES).find_in_batches do |blueprints|
-      blueprints.each do |blueprint|
-        blueprint.update_column(:preview, true)
-      end
-      sleep(0.1)
+    Blueprint.where(content_type: Blueprint::PREVIEW_CONTENT_TYPES).find_each do |blueprint|
+      blueprint.update_column(:preview, true)
     end
 
-    Blueprint.where(preview: nil).find_in_batches do |blueprints|
-      blueprints.each do |blueprint|
-        blueprint.update_column(:preview, false)
-      end
-      sleep(0.1)
+    Blueprint.where(preview: nil).find_each do |blueprint|
+      blueprint.update_column(:preview, false)
     end
   end
 
