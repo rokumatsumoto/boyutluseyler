4661f7555f6dba935a43b48aa2fc60513f25cebc
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-04-12 17:07:46 +0300

    Refactor FilenameHelpers class

--- lib/boyutluseyler/filename_helpers.rb
+++ lib/boyutluseyler/filename_helpers.rb
@@ -5,12 +5,16 @@ module Boyutluseyler
     # input: filename.jpg
     # output: filename
     def self.filename(name)
+      return nil if name.blank?
+
       File.basename(name, '.*').split('.')[0]
     end
 
     # input: filename.jpg
     # output: .jpg
     def self.extname(name)
+      return nil if name.blank?
+
       File.extname(name)
     end
   end
