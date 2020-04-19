ad4ea8a3d0ea3df03efc054fa70e3e42d37af244
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-04-19 13:20:04 +0300

    Add migrations for user avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_175438) do
+ActiveRecord::Schema.define(version: 2020_04_19_084438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -223,7 +223,7 @@ ActiveRecord::Schema.define(version: 2020_04_18_175438) do
   create_table "user_avatars", force: :cascade do |t|
     t.string "letter_avatar_url", null: false
     t.string "letter_avatar_thumb_url", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_avatars_on_user_id"
