ce628b1fe3a091272ddfb6d009544ef4409bc73d
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-04-24 19:43:45 +0300

    Refactor Ahoy::Event model

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -26,17 +26,28 @@ module Ahoy
 
     belongs_to :visit
     belongs_to :user, optional: true
+    belongs_to :design, class_name: 'Design', store: :properties, optional: true
+
     counter_culture :user,
                     column_name: proc { |e| user_event?(e.name) ? 'events_count' : nil },
                     touch: 'updated_at'
-
-    belongs_to :design, class_name: 'Design', store: :properties, optional: true
     counter_culture :design,
                     column_name: proc { |e| download_event?(e.name) ? 'downloads_count' : nil }
     counter_culture :design,
                     column_name: proc { |e| like_event?(e.name) ? 'likes_count' : nil }
 
     class << self
+      def cached_any_events_for?(event_name, user, event_properties)
+        _event_fk, event_id = event_properties.first # key, value = hash.first
+
+        cache_name = "any_events_for-#{event_name.parameterize.underscore}-#{event_id}-#{user.updated_at}"
+        Rails.cache.fetch(cache_name, expires_in: 1.week) do
+          Ahoy::Event.where(name: event_name, properties: event_properties, user_id: user.id).any?
+        end
+      end
+
+      private
+
       def user_event?(event_name)
         USER_EVENTS.include? event_name
       end
@@ -47,15 +58,6 @@ module Ahoy
 
       def like_event?(event_name)
         event_name == LIKED_DESIGN
-      end
-
-      def cached_any_events_for?(event_name, user, event_properties)
-        _event_fk, event_id = event_properties.first # key, value = hash.first
-
-        cache_name = "any_events_for-#{event_name.parameterize.underscore}-#{event_id}-#{user.updated_at}"
-        Rails.cache.fetch(cache_name, expires_in: 1.week) do
-          Ahoy::Event.where(name: event_name, properties: event_properties, user_id: user.id).any?
-        end
       end
     end
   end
