2b4c33b4d5e743aa5ba7bbdaa195b531faea9b83
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-04-26 23:59:50 +0300

    Refactor ValidatableFile concern

--- app/models/concerns/validatable_file.rb
+++ app/models/concerns/validatable_file.rb
@@ -3,6 +3,16 @@
 module ValidatableFile
   extend ActiveSupport::Concern
 
+  included do
+    validates :url, presence: true
+    validates :size, inclusion: { in: content_length_range }
+    validates :content_type, presence: true
+    validate :filename_is_blank
+
+    before_validation :sanitize_attrs
+    before_validation :ensure_content_type_correct
+  end
+
   class_methods do
     def content_length_range
       # Default values needed for testing factories in rails console
@@ -12,27 +22,13 @@ module ValidatableFile
     end
   end
 
-  included do
-    validate :filename_is_blank
-    validates :url, presence: true
-    validates :url_path, presence: true
-    validates :size, inclusion: { in: content_length_range }
-    validates :content_type, presence: true
-    before_validation :sanitize_attrs
-    before_validation :ensure_content_type_correct
-  end
-
   def filename_is_blank
     # user input: '.stl', '.png', '  .zip'
     errors.add(:url_path, :blank) if filename.blank?
   end
 
-  def filename
-    Boyutluseyler::FilenameHelpers.filename(url_path)
-  end
-
   def sanitize_attrs
-    # user input: '"><svg onload=alert(1)>.jpg'
+    # user input(XSS Svg payload): '"><svg onload=alert(1)>.jpg'
     %i[url url_path].each do |attr|
       value = self[attr]
       self[attr] = Sanitize.fragment(value) if value.present?
@@ -40,30 +36,24 @@ module ValidatableFile
   end
 
   def ensure_content_type_correct
-    invalid = content_type.blank? || allowed_content_types.none?(content_type)
+    invalid = content_type.blank? || allowed_content_types.exclude?(content_type)
 
     self.content_type = content_type_by_filename(url_path) if invalid
   end
 
-  def allowed_content_types
-    case model_name.name
-    when 'Illustration'
-      %w[image/png image/gif image/jpeg]
-    when 'Blueprint'
-      %w[application/vnd.ms-pki.stl
-         model/stl
-         application/x-tgif
-         application/zip]
-    else
-      []
-    end
-  end
-
   def content_type_by_filename(filename)
-    mime_info = MiniMime.lookup_by_filename(filename)
+    mime_info = MiniMime.lookup_by_filename(filename) if filename.present?
 
     return mime_info.content_type unless mime_info.nil?
 
     ''
+  end
+
+  def filename
+    Boyutluseyler::FilenameHelpers.filename(url_path)
+  end
+
+  def allowed_content_types
+    self.class::ALLOWED_CONTENT_TYPES
   end
 end
