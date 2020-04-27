07f015bbeff012fba5a7cb5b45c99a6cddd915e5
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-04-27 23:36:42 +0300

    Update Taggable concern

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -14,11 +14,14 @@ module Taggable
 
   # Split up the provided value by commas and (optional) spaces.
   def tags_as_string=(string)
-    self.tag_names = string.split(/,\s*/)
-    self.cached_tag_names = string
+    self.tag_names = string.split(/,\s*/).map(&:strip)
+
+    self.cached_tag_names = tags_as_string
   end
 
   def cached_tag_name_list
-    cached_tag_names.try(:split, /,\s*/) || []
+    return [] if cached_tag_names.blank?
+
+    cached_tag_names.split(/,\s*/)
   end
 end
