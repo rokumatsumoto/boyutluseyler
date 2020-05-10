b00f1becbc10947e4913f8b4e62e1c6e2306a906
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-05-10 22:58:24 +0300

    Add optional bucket dependency into Bucket

--- lib/object_storage/direct_upload/bucket.rb
+++ lib/object_storage/direct_upload/bucket.rb
@@ -7,16 +7,20 @@ require_relative 'presigned_post'
 module ObjectStorage
   module DirectUpload
     class Bucket
-      class << self
-        delegate :object, to: :bucket
+      def initialize(bucket = nil)
+        @bucket = bucket
+      end
 
-        def bucket
-          @bucket ||= DIRECT_UPLOAD_BUCKET
-        end
+      delegate :object, to: :bucket
 
-        def presigned_post(policy)
-          PresignedPost.new(bucket, policy).create
-        end
+      def presigned_post(policy)
+        PresignedPost.new(bucket, policy).create
+      end
+
+      private
+
+      def bucket
+        @bucket ||= DIRECT_UPLOAD_BUCKET
       end
     end
   end
