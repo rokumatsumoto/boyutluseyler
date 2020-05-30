e3f9c1cc4b09bf68954d4c86dccaba449534a046
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-05-30 22:52:30 +0300

    Refactor design creation process
    - Add Designs::AfterCreateService class
    - Move before_create code block into Designs::BuildService class
    - Refactor Designs::BaseService class

--- app/services/designs/create_service.rb
+++ app/services/designs/create_service.rb
@@ -5,8 +5,6 @@ module Designs
     def execute
       design = BuildService.new(nil, current_user, params).execute
 
-      before_create(design)
-
       success = design.save
 
       after_create(design) if success
@@ -16,22 +14,12 @@ module Designs
 
     private
 
-    def before_create(design)
-      design.model_file_format = model_file_format_for(design)
-    end
-
     def after_create(design)
-      popularity_score_service_for_design(design).execute
-
-      file_move_service_for_design(design).execute
+      after_create_service(design).execute
     end
 
-    def popularity_score_service_for_design(design)
-      Designs::PageViews::PopularityScoreService.new(design)
-    end
-
-    def file_move_service_for_design(design)
-      Designs::Files::MoveService.new(design, current_user, params)
+    def after_create_service(design)
+      AfterCreateService.new(design, current_user, params)
     end
   end
 end
