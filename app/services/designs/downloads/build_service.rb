cc9c1e2de56e21e604d6e1c095a7e412af17e5a5
Samet Gunaydin <rokumatsumoto@gmail.com>
2020-05-30 23:05:45 +0300

    Create design download with not_ready step

--- app/services/designs/downloads/build_service.rb
+++ app/services/designs/downloads/build_service.rb
@@ -4,8 +4,8 @@ module Designs
   module Downloads
     class BuildService < Designs::BaseService
       def execute
-        DesignDownload.new(params).tap do |dd|
-          dd.design = design
+        DesignDownload.new(params).tap do |design_download|
+          design_download.design = design
         end
       end
     end
